254826498770ba87b3b122b812abb50f95f4338d
EmmanuelOgbewe <emmanuel.ogbewe12@gmail.com>
2020-03-17 20:49:03 -0400

    Updates

---
+++ precache-manifest.e9839139a24c279e251e24435876bbe2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b857e0518e3ad0b0afbe48e6957cd829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5192d34beaae5d81e613",
+    "url": "/static/css/main.9386a072.chunk.css"
+  },
+  {
+    "revision": "f29be1f916f6844bf4ff",
+    "url": "/static/js/2.5096b936.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.5096b936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5192d34beaae5d81e613",
+    "url": "/static/js/main.1323c5ba.chunk.js"
+  },
+  {
+    "revision": "400b3f58a23bf9fede4f",
+    "url": "/static/js/runtime-main.c4d0fa9f.js"
+  },
+  {
+    "revision": "d299dd94cea495620e8b5872871d3545",
+    "url": "/static/media/nxtgame_landing_page.d299dd94.png"
+  },
+  {
+    "revision": "450b7dcb3fe1d5dd40cc7f37b44d60bf",
+    "url": "/static/media/project_nxtgame@3x.450b7dcb.png"
+  }
+]);
